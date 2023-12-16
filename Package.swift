c23b3ce81f485f7c902da61a395f002110e16eab
abdullahsonmez49 <sonmezabdullahh@gmail.com>
2023-12-17 02:00:32 +0300

    2.7.2

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "IubendaMobileSDK",
-            targets: ["IubendaMobileSDK"]),
+            name: "iubenda",
+            targets: ["iubenda"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IubendaMobileSDK",
+            name: "iubenda",
             path: "iubenda.xcframework",
             publicHeadersPath: "."
         ),
