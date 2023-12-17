7eb5c9735db0cad87beb222add248a0cb2a1c019
abdullahsonmez49 <sonmezabdullahh@gmail.com>
2023-12-17 11:41:02 +0300

    sfbv

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "iubenda",
-            path: "iubenda.xcframework",
-            publicHeadersPath: "."
+            path: "iubenda.xcframework"
         )
     ]
 )
