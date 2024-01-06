078e2b513a24087a7ea6400f92478c14ac382749
Abdullah Sönmez <99615897+abdullahsonmez49@users.noreply.github.com>
2024-01-07 00:30:18 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "iubenda",
-            targets: ["iubenda"]),
+            targets: ["iubenda"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
