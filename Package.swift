537399c9a304ac447d3fc40831774c877ea134d9
Abdullah Sönmez <99615897+abdullahsonmez49@users.noreply.github.com>
2024-01-07 00:41:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "iubenda",
-            path: "iubenda.xcframework"
+            path: "iubenda.xcframework",
+            publicHeadersPath: "sdk"
         )
     ]
 )
